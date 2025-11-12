699a3808176f44ad292e1146367a6d3f3540b42c
Shun Ueda <me@shu.nu>
2025-11-12 09:11:20 -0500

    refactor: optimize for multilang (#11)

--- scripts/generate.ts
+++ scripts/generate.ts
@@ -1,3 +1,4 @@
+import { log, warn } from 'node:console'
 import { join } from 'node:path'
 import { generateProviderConstruct, type Language } from '../src/cdktf.ts'
 import {
@@ -8,33 +9,32 @@ import {
 // TypeScript only for now, but code is ready for more languages
 const languages: Language[] = ['typescript']
 
-for (const language of languages) {
-  for await (const { attributes, id } of fetchProviderData()) {
-    const { name, ['full-name']: fullname } = attributes
+for await (const { attributes, id } of fetchProviderData()) {
+  const { name, ['full-name']: fullname } = attributes
 
-    const dir = join('gen', fullname, language)
+  const versionData = await fetchProviderVerionData(id)
+  const version = versionData
+    .map(it => it.attributes)
+    .filter(({ version }) => /^\d+\.\d+\.\d+$/.test(version)) // only x.x.x versions
+    .toSorted((a, b) => b['published-at'].localeCompare(a['published-at'])) // latest
+    ?.at(0)?.version
+
+  if (!version) {
+    warn(fullname, `No valid version found, skipping.`)
+    continue
+  }
+
+  for (const language of languages) {
+    const directory = join('gen', fullname, language)
     const prefix = `[${fullname} ${language}]`
-
-    const versionData = await fetchProviderVerionData(id)
-    const version = versionData
-      .map(it => it.attributes)
-      .filter(({ version }) => /^\d+\.\d+\.\d+$/.test(version)) // only x.x.x versions
-      .toSorted((a, b) => b['published-at'].localeCompare(a['published-at'])) // latest
-      ?.at(0)?.version
-
-    if (!version) {
-      console.warn(prefix, `No valid version found, skipping.`)
-      continue
-    }
-
     try {
-      console.log(prefix, 'Generating construct...')
+      log(prefix, 'Generating construct...')
       await generateProviderConstruct({
         language,
         name,
         version,
         source: fullname,
-        directory: dir
+        directory
       })
       console.log(prefix, 'Done.')
     } catch (error) {
